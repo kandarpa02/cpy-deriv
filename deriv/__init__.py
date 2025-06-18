367bce55a6c36bee37c7b08d8fe4c62cab75a964
kandarpa02 <kandarpaexe@gmail.com>
2025-06-18 11:56:25 +0530

    added regulate_parent mode

--- deriv/__init__.py
+++ deriv/__init__.py
@@ -1,6 +1,6 @@
 from .Array.array_object import *
 from .Array.AMath import *
 from .Array._condition import *
-from .Array.static import XLR
+from .Array.static import compact
 from .helpers.grad_enabler import grads_on
 from .nn import ReLU, Tanh, Nami
